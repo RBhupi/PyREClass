d3a707578b64be910aa9a998ba42a6e164edd968
Valentin Louf <v.louf@bom.gov.au>
2020-02-13 11:26:01 +0800

    Modification of the example

--- example/example.py
+++ example/example.py
@@ -1,31 +1,27 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Oct 19 14:40:07 2019
 @author: Bhupendra Raut
+@modified: Valentin Louf
+@modification: Thu Feb 13 11:23:00 2020
 """
-
-from netCDF4 import Dataset
 import numpy as np
-from matplotlib import pyplot as plot
-from wtclass import getWTClass
-
-import matplotlib.cm as cm
-
-
+import netCDF4
+import matplotlib.pyplot as plt
+from pyreclass import getWTClass
 
 fname = "/home/bhupendra/projects/screim/data/testdata/CPOL_20161210_0730_GRIDS_2500m.nc"
 
-nc = Dataset(fname, "r")
-data2d = np.squeeze(nc.variables["corrected_reflectivity"][0, 6, :, :]) #data*
-
-plot.matshow(data2d)
-
-
-data2d=data2d.filled(0)
-data2d[data2d<0]=0
+with netCDF4.Dataset(fname, "r") as ncid:
+    data2d = np.squeeze(ncid["corrected_reflectivity"][0, 6, :, :]) #data*
+    data2d = data2d.filled(0)
+    data2d[data2d < 0] = 0
 
 wt_class = getWTClass(data2d, res_km=2.5, conv_scale_km=20)
 
-plot.matshow(wt_class)
-plot.colorbar()
+fig, ax = pl.subplots(1, 2, figsize=(12, 5))
+ax = ax.ravel()
+
+ax[0].pcolormesh(refl2d)
+ax[1].pcolormesh(wt_class)
+
+pl.show()
